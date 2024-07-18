846810c424395acb1ee1a58070dce85b5709b467
bagenigilbert <bagenigilbert@gmail.com>
2024-07-18 15:56:22 +0300

    grabbed elements for form display

---
+++ photogallary/photos.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const uploadForm = document.getElementById("upload-form");
+    const uploadInput = document.getElementById("upload-input");
+    const albumNameInput = document.getElementById("album-name");
+    const photoGrid = document.getElementById("photo-grid");
+    const albumGrid = document.getElementById("album-grid");
+
+    // array to store all uploads photos
+    const photos = [];
+
+    //object to store albums and their photos
+    const albums = {};
+
+    // function to display photos in the grid
+    const displayPhotos = (photo) =>{
+        //create a div element to hold photos and display 
+        
+    }
+})
